ea96ca382e4af4de9a0e4bea5890898a7f85e0ff
rjq2 <roberto.kratox@gmail.com>
2024-06-28 02:22:36 -0400

    Add files via upload

--- markov_chains.py
+++ markov_chains.py
@@ -349,8 +349,8 @@ def markov_transition_biro(pref_1, pref_2, MatchingM, actual_match_M):
     blocking_pair_3 = blocking_pair.copy()
 
     for i in range(n_f):
-        if np.sum(blocking_pair_3[i, :] == 1) > 1:
-            a = np.where(blocking_pair_3[i, :] == 1)[0]
+        if np.sum(blocking_pair_3[:, i] == 1) > 1:
+            a = np.where(blocking_pair_3[:, i] == 1)[0]
             if n == 2:
                 blocking_pair_3[:, i] = 0
                 blocking_pair_3[np.argmax(pref_2[:, i]), i] = 1
